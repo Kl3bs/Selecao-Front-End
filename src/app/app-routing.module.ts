2d51199ff41b482ddfcb35ffda1fb68423cad231
Kleber Vasconcelos <kleberjunior58@gmail.com>
2021-08-13 20:28:31 -0300

    Component update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FormComponent } from './form/form.component';
+import { ListAllComponent } from './list-all/list-all.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'list', component: ListAllComponent },
+  {path: 'form', component: FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {IMaskModule} from 'angular-imask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormComponent } from './form/form.component';
+import { ListAllComponent } from './list-all/list-all.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListAllComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    IMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
